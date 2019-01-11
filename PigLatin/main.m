17b12e52575491465e1118ac0e972bfcb1993746
Jun Oh <kyungjunjoh@gmail.com>
2019-01-11 14:06:50 -0800

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Jun Oh on 2019-01-11.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
